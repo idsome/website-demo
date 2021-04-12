6c7f7cef41af5bbb76a0ae62e0868149549cbab8
idsome99 <mohmedismail37@yahoo.com>
2021-04-12 20:19:47 +0300

    jquery-update

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#logo").click(function(){
+        alert("this logo is just a demo");
+    
+    });
+    $("#side-links").click(function(){
+        alert("this is a dummy link , it doesn't take u anywhere!")
+
+    });
+    $("#close").click(function(){
+        $("#side-bar").hide();
+    });
+});
